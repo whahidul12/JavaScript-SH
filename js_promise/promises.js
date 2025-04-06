912412d0504c5f403328b1ac8d89f7eebabf748a
whahidul12 <mdislamwhahid@gmail.com>
2025-04-07 02:59:24 +0600

    add promises

--- js_promise/promises.js
+++ js_promise/promises.js
@@ -3,12 +3,44 @@ const myPromise = new Promise((res, rej) => {
         console.log("inner pronise")
     })()
     setTimeout(() => {
-        console.log("settimeout")
-        res()
+        const err = true
+        if (!err) {
+            res("everything is okay!!!")
+        } else {
+            rej("something is worng")
+        }
     }, 3000);
 })
 
-myPromise.then(() => {
-    console.log("outter promise");
+// myPromise.then((e) => {
+//     console.log("promise:", e);
+// }).catch((e) => {
+//     console.log(e);
+// }).finally(() => {
+//     count = 1 + 11;
+//     console.log(">>>", count);
+// })
 
-})
+async function myAsycAwit() {
+    try {
+        const response = await myPromise
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+myAsycAwit()
+
+async function anotherAysc() {
+    try {
+        const response = await fetch("https://api.github.com/users/whahidul12")
+        const data = await response.json();
+        console.log(data);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherAysc()
